7d45c39f826736e372c514aa077d4e1ec809aa6c
pumkinni <102509636+pumkinni@users.noreply.github.com>
2023-01-11 22:02:21 +0900

    Greedy Practice2

--- GreedyPractice2.java
+++ GreedyPractice2.java
@@ -1,15 +1,4 @@
-// Practice
-// 양의 정수 배열 prices 가 주어졌다.
-// 각 원소의 의미는 주식 가격을 의미한다.
-// 한 번에 한 주만 보유할 수 있다고 할 때 prices 를 보고 사고 팔기를 반복해서
-// 얻을 수 있는 최대 수익을 반환하는 프로그램을 작성하세요.
 
-// 입출력 예시
-// prices: 5, 1, 6, 4, 3, 5
-// 출력: 7
-
-// prices: 1, 2, 3, 4, 5
-// 출력: 4
 
 public class GreedyPractice2 {
     public static int solution(int[] prices) {
